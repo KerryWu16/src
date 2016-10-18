a2c5431c64ee7616c2dd967b18d91ccda6d57555
Beck <pshfls@gmail.com>
2016-10-18 18:09:46 +0800

    finish controller for hover, need testing

--- proj1phase3/your_code/control_function.h
+++ proj1phase3/your_code/control_function.h
@@ -35,9 +35,20 @@ void SO3Control_function( const double des_pos[3],
                           double &target_thrust
                         )
 {
+	double K_px = 6;
+	double K_dx = 4.5;
+	double K_py = 6;
+	double K_dy = 4.5;
+	double K_pz = 50.0;
+	double K_dz = 80.0;
+
+	double r_1_acc = des_acc[1] + K_dx * (des_vel[1] - now_vel[1]) + K_px * (des_pos[1] - now_pos[1]);
+	double r_2_acc = des_acc[2] + K_dy * (des_vel[2] - now_vel[2]) + K_py * (des_pos[2] - now_pos[2]); 
+	double r_3_acc = des_acc[3] + K_dz * (des_vel[3] - now_vel[3]) + K_pz * (des_pos[3] - now_pos[3]);
+	
+	rpy[0] = (r_1_acc * sin(now_yaw) - r_2_acc * cos(now_yaw)) / Gravity;	
+	rpy[1] = (r_1_acc * cos(now_yaw) + r_2_acc * sin(now_yaw)) / Gravity;
 	rpy[2] = 0.0;
-	rpy[1] = 0.0;
-	rpy[0] = 0.11;	
-	target_thrust = Mass*(Gravity + des_acc[3]);       
+	target_thrust = Mass*(Gravity + r_3_acc);       
 }
 #endif
