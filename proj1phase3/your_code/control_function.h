e5c8d788fcdddd62058d3a6c0d78e4fcc030637d
Beck <pshfls@gmail.com>
2016-10-20 14:47:50 +0800

    use the constants from ROS and printout debug

--- proj1phase3/your_code/control_function.h
+++ proj1phase3/your_code/control_function.h
@@ -35,23 +35,15 @@ void SO3Control_function( const double des_pos[3],
                           double &target_thrust
                         )
 {
-	// Constants
-	double K_px = 6;
-	double K_dx = 4.5;
-	double K_py = 6;
-	double K_dy = 4.5;
-	double K_pz = 50.0;
-	double K_dz = 80.0;
-
 	double r_acc[3];
-	r_acc[0] = des_acc[0] + K_dx * (des_vel[0] - now_vel[0]) + K_px * (des_pos[0] - now_pos[0]);
-	r_acc[1] = des_acc[1] + K_dy * (des_vel[1] - now_vel[1]) + K_py * (des_pos[1] - now_pos[1]); 
-	r_acc[2] = des_acc[2] + K_dz * (des_vel[2] - now_vel[2]) + K_pz * (des_pos[2] - now_pos[2]);
+	r_acc[0] = des_acc[0] + Kd[0] * (des_vel[0] - now_vel[0]) + Kp[0] * (des_pos[0] - now_pos[0]);
+	r_acc[1] = des_acc[1] + Kd[1] * (des_vel[1] - now_vel[1]) + Kp[1] * (des_pos[1] - now_pos[1]); 
+	r_acc[2] = des_acc[2] + Kd[2] * (des_vel[2] - now_vel[2]) + Kp[2] * (des_pos[2] - now_pos[2]);
+	cout << "designated x accel:" << r_acc[0] << "y accel:" << r_acc[1] << "z accel:" << r_acc[2] << ".\n";
 	
 	rpy[0] = (r_acc[0] * sin(now_yaw) - r_acc[1] * cos(now_yaw)) / Gravity;	
 	rpy[1] = (r_acc[0] * cos(now_yaw) + r_acc[1] * sin(now_yaw)) / Gravity;
 	rpy[2] = 0.0;
-
 	target_thrust = Mass*(Gravity + r_acc[2]);       
 }
 #endif
